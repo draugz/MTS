bd7311f720859f4e6373443d6f07f4ffd4115b1d
Edgars Grinvalds <edgars.grinvalds@gmail.com>
2020-01-06 23:16:33 +0200

    spring init

---
+++ rest-api/src/test/java/lv/brain/dashboard/mts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.brain.dashboard.mts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
